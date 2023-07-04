21384e7375bcb5e2d9daa73bdf0ed83f7339543f
Vishnu8991 <vishnups8991@gmail.com>
2023-07-04 12:19:50 +0530

    Commit

---
+++ bin/Collectionsss/list_ex.dart
@@ -0,0 +1,69 @@
+// List
+//2. types : 
+
+// 1. fixed length list
+// 2. growable list
+
+void main() {
+///1.dynamic / literal method for list creation 
+List myList = [1,2,3,4,5,6,7,"king"];  // this is a dynamic list
+List <int> numbers = [10,20,21,23];
+
+// NOTE : if you want to store specific type data then use List<datatype> listname   DO NOT USE var
+
+
+
+// 2. List.empty()
+List l1 = List.empty();  //this will create an empty list eg: []  (bcoz growable is false so cannot add values)
+// l1.add(10);           //growable is false so cannot add values
+
+List l2 = List.empty(growable: true);
+l2.add(11);
+l2.addAll(numbers);
+l2.insert(1,50);
+
+
+
+
+///3. List.from
+List l3 = List.from([1,3,5,7,9]);
+l3.addAll(numbers);
+
+
+
+///4. List.of
+List l4 = List.of([2,4,6,8,9]);
+// l4.addAll(numbers);
+l4.addAll(l3);
+
+
+
+///5.List.filled
+List l5 = List.filled(10, 2,);
+l5[1] = 3;
+l5[1] = 5;
+///l5.add(10); cannot add to a fixed list since l5 has a intial value of growable = false
+
+
+///6. List.generate
+List l6 = List.generate(5, (index) => 8 * index+2);  //8 * 0 + 2   8 * 2 + 2 etc
+
+
+
+//7. List.unmodifiable
+List l7 = List.unmodifiable(l6);   // values from l6 added to l7
+// l7.add(15); cannot add 
+
+
+
+
+print('literal list = $myList');
+print('literal list2 = $numbers');
+print("l1 = $l1");
+print("l2 = $l2");
+print("l3 = $l3");
+print("l4 = $l4");
+print("l5 = $l5");
+print("l6 = $l6");
+print("l7 = $l7");
+}
--- bin/constructor.dart
+++ bin/constructor.dart
@@ -1,16 +1,47 @@
 class A{
 
-  // constructor  is a  special type function which name is same as class name does not have a return type
+      // constructor  is a  special type function which name is same as class name does not have a return type
+    //does not have a return type
+  //name is same as class name
 
+  ///default constructor
   A(){
     print("Constructor");
   }
+
+
+  ///parameterised  constructor
+  // A(int a, String b){
+  //   print('parameterised constructor $a, $b');
+  // }
+
+  ///named constructor
+  A.con1(){
+    print('default named constructor');
+  }
+
+  ///named constructor with optional parameters
+  A.con2(int a, {int? n}){
+    print('parameterised named constructor $a, $n');
+  }
+
+
   void show(){
     print("inside show function");
   }
 }
 
   void main () {
+
+    ///constructor is invoked or executed automatically
+    ///when the object is created
     A obj = A();
     obj.show();
+    A obj1 = A.con1();
+    A obj2 = A.con2(100, n: 0);
+  
+    obj1.show();
+    obj2.show();
   }
+
+  ///DO NOT USE DEFAULT AND PARAMETERISED CONSTRUCTOR TOGETHER WITHIN A CLASS
